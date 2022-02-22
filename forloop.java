7210880c74f54e9c965c73d7892efe316deffac1
Piyush Bheda <piyushbheda7@gmail.com>
2022-02-22 10:09:03 +0530

    forloop

---
+++ forloop.java
@@ -0,0 +1,12 @@
+package forloop;
+
+public class forloop
+{
+    public static void main(String args[])
+    {
+        for(int i = 0 ; i < 10 ; i++)
+        {
+            System.out.println("Hello World!  : "+i);
+        }
+    }
+}
